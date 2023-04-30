7f36487ea2acae301c7b9a97db26c1d27c9323b6
unknown <sireesharoyals4@gmail.com>
2023-04-30 15:47:42 +0530

    first project

---
+++ demo2/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>my name is siri </h1>
+      <h1>my  friend name is ashu </h1>
+
+    </div>
+  );
+}
+
+export default App;
